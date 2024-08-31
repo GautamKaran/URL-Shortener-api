8ea10d743ee37b8f3c6a92d9d2a6cecd56eb7eb9
gautamkaran <karankumargautam.dev@gmail.com>
2024-08-31 11:27:50 +0530

    cors config

--- src/app.js
+++ src/app.js
@@ -1,5 +1,13 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
 
 export { app };
